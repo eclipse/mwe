7d2ef7943c14583655e1fefa0ebecd556d5c42b8
pschonbac <pschonbac>
2008-02-22 16:41:23 +0000

    EMF model of workflow added

---
+++ plugins/org.eclipse.emf.mwe.ui.newEditor/src-gen/workflow/ObjectValue.java
@@ -0,0 +1,53 @@
+/**
+ * <copyright>
+ * </copyright>
+ *
+ * $Id: ObjectValue.java,v 1.1 2008/02/22 16:41:24 pschonbac Exp $
+ */
+package workflow;
+
+
+/**
+ * <!-- begin-user-doc -->
+ * A representation of the model object '<em><b>Object Value</b></em>'.
+ * <!-- end-user-doc -->
+ *
+ * <p>
+ * The following features are supported:
+ * <ul>
+ *   <li>{@link workflow.ObjectValue#getClazz <em>Clazz</em>}</li>
+ * </ul>
+ * </p>
+ *
+ * @see workflow.WorkflowPackage#getObjectValue()
+ * @model
+ * @generated
+ */
+public interface ObjectValue extends ComplexValue {
+    /**
+     * Returns the value of the '<em><b>Clazz</b></em>' attribute.
+     * <!-- begin-user-doc -->
+     * <p>
+     * If the meaning of the '<em>Clazz</em>' attribute isn't clear,
+     * there really should be more of a description here...
+     * </p>
+     * <!-- end-user-doc -->
+     * @return the value of the '<em>Clazz</em>' attribute.
+     * @see #setClazz(String)
+     * @see workflow.WorkflowPackage#getObjectValue_Clazz()
+     * @model
+     * @generated
+     */
+    String getClazz();
+
+    /**
+     * Sets the value of the '{@link workflow.ObjectValue#getClazz <em>Clazz</em>}' attribute.
+     * <!-- begin-user-doc -->
+     * <!-- end-user-doc -->
+     * @param value the new value of the '<em>Clazz</em>' attribute.
+     * @see #getClazz()
+     * @generated
+     */
+    void setClazz(String value);
+
+} // ObjectValue
---
+++ plugins/org.eclipse.emf.mwe.ui.newEditor/src-gen/workflow/impl/WorkflowPackageImpl.java
@@ -0,0 +1,538 @@
+/**
+ * <copyright>
+ * </copyright>
+ *
+ * $Id: WorkflowPackageImpl.java,v 1.1 2008/02/22 16:41:24 pschonbac Exp $
+ */
+package workflow.impl;
+
+import org.eclipse.emf.ecore.EAttribute;
+import org.eclipse.emf.ecore.EClass;
+import org.eclipse.emf.ecore.EPackage;
+import org.eclipse.emf.ecore.EReference;
+
+import org.eclipse.emf.ecore.impl.EPackageImpl;
+
+import workflow.AbstractValue;
+import workflow.Assignment;
+import workflow.ComplexValue;
+import workflow.FileProperty;
+import workflow.FileValue;
+import workflow.ObjectValue;
+import workflow.Property;
+import workflow.ReferencedValue;
+import workflow.SimpleProperty;
+import workflow.SimpleValue;
+import workflow.WorkflowFactory;
+import workflow.WorkflowFile;
+import workflow.WorkflowPackage;
+
+/**
+ * <!-- begin-user-doc -->
+ * An implementation of the model <b>Package</b>.
+ * <!-- end-user-doc -->
+ * @generated
+ */
+public class WorkflowPackageImpl extends EPackageImpl implements WorkflowPackage {
+    /**
+     * <!-- begin-user-doc -->
+     * <!-- end-user-doc -->
+     * @generated
+     */
+    private EClass workflowFileEClass = null;
+
+    /**
+     * <!-- begin-user-doc -->
+     * <!-- end-user-doc -->
+     * @generated
+     */
+    private EClass propertyEClass = null;
+
+    /**
+     * <!-- begin-user-doc -->
+     * <!-- end-user-doc -->
+     * @generated
+     */
+    private EClass simplePropertyEClass = null;
+
+    /**
+     * <!-- begin-user-doc -->
+     * <!-- end-user-doc -->
+     * @generated
+     */
+    private EClass filePropertyEClass = null;
+
+    /**
+     * <!-- begin-user-doc -->
+     * <!-- end-user-doc -->
+     * @generated
+     */
+    private EClass assignmentEClass = null;
+
+    /**
+     * <!-- begin-user-doc -->
+     * <!-- end-user-doc -->
+     * @generated
+     */
+    private EClass abstractValueEClass = null;
+
+    /**
+     * <!-- begin-user-doc -->
+     * <!-- end-user-doc -->
+     * @generated
+     */
+    private EClass complexValueEClass = null;
+
+    /**
+     * <!-- begin-user-doc -->
+     * <!-- end-user-doc -->
+     * @generated
+     */
+    private EClass simpleValueEClass = null;
+
+    /**
+     * <!-- begin-user-doc -->
+     * <!-- end-user-doc -->
+     * @generated
+     */
+    private EClass objectValueEClass = null;
+
+    /**
+     * <!-- begin-user-doc -->
+     * <!-- end-user-doc -->
+     * @generated
+     */
+    private EClass referencedValueEClass = null;
+
+    /**
+     * <!-- begin-user-doc -->
+     * <!-- end-user-doc -->
+     * @generated
+     */
+    private EClass fileValueEClass = null;
+
+    /**
+     * Creates an instance of the model <b>Package</b>, registered with
+     * {@link org.eclipse.emf.ecore.EPackage.Registry EPackage.Registry} by the package
+     * package URI value.
+     * <p>Note: the correct way to create the package is via the static
+     * factory method {@link #init init()}, which also performs
+     * initialization of the package, or returns the registered package,
+     * if one already exists.
+     * <!-- begin-user-doc -->
+     * <!-- end-user-doc -->
+     * @see org.eclipse.emf.ecore.EPackage.Registry
+     * @see workflow.WorkflowPackage#eNS_URI
+     * @see #init()
+     * @generated
+     */
+    private WorkflowPackageImpl() {
+        super(eNS_URI, WorkflowFactory.eINSTANCE);
+    }
+
+    /**
+     * <!-- begin-user-doc -->
+     * <!-- end-user-doc -->
+     * @generated
+     */
+    private static boolean isInited = false;
+
+    /**
+     * Creates, registers, and initializes the <b>Package</b> for this
+     * model, and for any others upon which it depends.  Simple
+     * dependencies are satisfied by calling this method on all
+     * dependent packages before doing anything else.  This method drives
+     * initialization for interdependent packages directly, in parallel
+     * with this package, itself.
+     * <p>Of this package and its interdependencies, all packages which
+     * have not yet been registered by their URI values are first created
+     * and registered.  The packages are then initialized in two steps:
+     * meta-model objects for all of the packages are created before any
+     * are initialized, since one package's meta-model objects may refer to
+     * those of another.
+     * <p>Invocation of this method will not affect any packages that have
+     * already been initialized.
+     * <!-- begin-user-doc -->
+     * <!-- end-user-doc -->
+     * @see #eNS_URI
+     * @see #createPackageContents()
+     * @see #initializePackageContents()
+     * @generated
+     */
+    public static WorkflowPackage init() {
+        if (isInited) return (WorkflowPackage)EPackage.Registry.INSTANCE.getEPackage(WorkflowPackage.eNS_URI);
+
+        // Obtain or create and register package
+        WorkflowPackageImpl theWorkflowPackage = (WorkflowPackageImpl)(EPackage.Registry.INSTANCE.getEPackage(eNS_URI) instanceof WorkflowPackageImpl ? EPackage.Registry.INSTANCE.getEPackage(eNS_URI) : new WorkflowPackageImpl());
+
+        isInited = true;
+
+        // Create package meta-data objects
+        theWorkflowPackage.createPackageContents();
+
+        // Initialize created meta-data
+        theWorkflowPackage.initializePackageContents();
+
+        // Mark meta-data to indicate it can't be changed
+        theWorkflowPackage.freeze();
+
+        return theWorkflowPackage;
+    }
+
+    /**
+     * <!-- begin-user-doc -->
+     * <!-- end-user-doc -->
+     * @generated
+     */
+    public EClass getWorkflowFile() {
+        return workflowFileEClass;
+    }
+
+    /**
+     * <!-- begin-user-doc -->
+     * <!-- end-user-doc -->
+     * @generated
+     */
+    public EReference getWorkflowFile_Assignment() {
+        return (EReference)workflowFileEClass.getEStructuralFeatures().get(0);
+    }
+
+    /**
+     * <!-- begin-user-doc -->
+     * <!-- end-user-doc -->
+     * @generated
+     */
+    public EReference getWorkflowFile_Properties() {
+        return (EReference)workflowFileEClass.getEStructuralFeatures().get(1);
+    }
+
+    /**
+     * <!-- begin-user-doc -->
+     * <!-- end-user-doc -->
+     * @generated
+     */
+    public EClass getProperty() {
+        return propertyEClass;
+    }
+
+    /**
+     * <!-- begin-user-doc -->
+     * <!-- end-user-doc -->
+     * @generated
+     */
+    public EClass getSimpleProperty() {
+        return simplePropertyEClass;
+    }
+
+    /**
+     * <!-- begin-user-doc -->
+     * <!-- end-user-doc -->
+     * @generated
+     */
+    public EAttribute getSimpleProperty_Name() {
+        return (EAttribute)simplePropertyEClass.getEStructuralFeatures().get(0);
+    }
+
+    /**
+     * <!-- begin-user-doc -->
+     * <!-- end-user-doc -->
+     * @generated
+     */
+    public EAttribute getSimpleProperty_Value() {
+        return (EAttribute)simplePropertyEClass.getEStructuralFeatures().get(1);
+    }
+
+    /**
+     * <!-- begin-user-doc -->
+     * <!-- end-user-doc -->
+     * @generated
+     */
+    public EClass getFileProperty() {
+        return filePropertyEClass;
+    }
+
+    /**
+     * <!-- begin-user-doc -->
+     * <!-- end-user-doc -->
+     * @generated
+     */
+    public EAttribute getFileProperty_File() {
+        return (EAttribute)filePropertyEClass.getEStructuralFeatures().get(0);
+    }
+
+    /**
+     * <!-- begin-user-doc -->
+     * <!-- end-user-doc -->
+     * @generated
+     */
+    public EClass getAssignment() {
+        return assignmentEClass;
+    }
+
+    /**
+     * <!-- begin-user-doc -->
+     * <!-- end-user-doc -->
+     * @generated
+     */
+    public EAttribute getAssignment_ToAssignTo() {
+        return (EAttribute)assignmentEClass.getEStructuralFeatures().get(0);
+    }
+
+    /**
+     * <!-- begin-user-doc -->
+     * <!-- end-user-doc -->
+     * @generated
+     */
+    public EReference getAssignment_Value() {
+        return (EReference)assignmentEClass.getEStructuralFeatures().get(1);
+    }
+
+    /**
+     * <!-- begin-user-doc -->
+     * <!-- end-user-doc -->
+     * @generated
+     */
+    public EClass getAbstractValue() {
+        return abstractValueEClass;
+    }
+
+    /**
+     * <!-- begin-user-doc -->
+     * <!-- end-user-doc -->
+     * @generated
+     */
+    public EClass getComplexValue() {
+        return complexValueEClass;
+    }
+
+    /**
+     * <!-- begin-user-doc -->
+     * <!-- end-user-doc -->
+     * @generated
+     */
+    public EAttribute getComplexValue_Name() {
+        return (EAttribute)complexValueEClass.getEStructuralFeatures().get(0);
+    }
+
+    /**
+     * <!-- begin-user-doc -->
+     * <!-- end-user-doc -->
+     * @generated
+     */
+    public EReference getComplexValue_Assignment() {
+        return (EReference)complexValueEClass.getEStructuralFeatures().get(1);
+    }
+
+    /**
+     * <!-- begin-user-doc -->
+     * <!-- end-user-doc -->
+     * @generated
+     */
+    public EClass getSimpleValue() {
+        return simpleValueEClass;
+    }
+
+    /**
+     * <!-- begin-user-doc -->
+     * <!-- end-user-doc -->
+     * @generated
+     */
+    public EAttribute getSimpleValue_Val() {
+        return (EAttribute)simpleValueEClass.getEStructuralFeatures().get(0);
+    }
+
+    /**
+     * <!-- begin-user-doc -->
+     * <!-- end-user-doc -->
+     * @generated
+     */
+    public EClass getObjectValue() {
+        return objectValueEClass;
+    }
+
+    /**
+     * <!-- begin-user-doc -->
+     * <!-- end-user-doc -->
+     * @generated
+     */
+    public EAttribute getObjectValue_Clazz() {
+        return (EAttribute)objectValueEClass.getEStructuralFeatures().get(0);
+    }
+
+    /**
+     * <!-- begin-user-doc -->
+     * <!-- end-user-doc -->
+     * @generated
+     */
+    public EClass getReferencedValue() {
+        return referencedValueEClass;
+    }
+
+    /**
+     * <!-- begin-user-doc -->
+     * <!-- end-user-doc -->
+     * @generated
+     */
+    public EAttribute getReferencedValue_Ref() {
+        return (EAttribute)referencedValueEClass.getEStructuralFeatures().get(0);
+    }
+
+    /**
+     * <!-- begin-user-doc -->
+     * <!-- end-user-doc -->
+     * @generated
+     */
+    public EClass getFileValue() {
+        return fileValueEClass;
+    }
+
+    /**
+     * <!-- begin-user-doc -->
+     * <!-- end-user-doc -->
+     * @generated
+     */
+    public EAttribute getFileValue_File() {
+        return (EAttribute)fileValueEClass.getEStructuralFeatures().get(0);
+    }
+
+    /**
+     * <!-- begin-user-doc -->
+     * <!-- end-user-doc -->
+     * @generated
+     */
+    public WorkflowFactory getWorkflowFactory() {
+        return (WorkflowFactory)getEFactoryInstance();
+    }
+
+    /**
+     * <!-- begin-user-doc -->
+     * <!-- end-user-doc -->
+     * @generated
+     */
+    private boolean isCreated = false;
+
+    /**
+     * Creates the meta-model objects for the package.  This method is
+     * guarded to have no affect on any invocation but its first.
+     * <!-- begin-user-doc -->
+     * <!-- end-user-doc -->
+     * @generated
+     */
+    public void createPackageContents() {
+        if (isCreated) return;
+        isCreated = true;
+
+        // Create classes and their features
+        workflowFileEClass = createEClass(WORKFLOW_FILE);
+        createEReference(workflowFileEClass, WORKFLOW_FILE__ASSIGNMENT);
+        createEReference(workflowFileEClass, WORKFLOW_FILE__PROPERTIES);
+
+        propertyEClass = createEClass(PROPERTY);
+
+        simplePropertyEClass = createEClass(SIMPLE_PROPERTY);
+        createEAttribute(simplePropertyEClass, SIMPLE_PROPERTY__NAME);
+        createEAttribute(simplePropertyEClass, SIMPLE_PROPERTY__VALUE);
+
+        filePropertyEClass = createEClass(FILE_PROPERTY);
+        createEAttribute(filePropertyEClass, FILE_PROPERTY__FILE);
+
+        assignmentEClass = createEClass(ASSIGNMENT);
+        createEAttribute(assignmentEClass, ASSIGNMENT__TO_ASSIGN_TO);
+        createEReference(assignmentEClass, ASSIGNMENT__VALUE);
+
+        abstractValueEClass = createEClass(ABSTRACT_VALUE);
+
+        complexValueEClass = createEClass(COMPLEX_VALUE);
+        createEAttribute(complexValueEClass, COMPLEX_VALUE__NAME);
+        createEReference(complexValueEClass, COMPLEX_VALUE__ASSIGNMENT);
+
+        simpleValueEClass = createEClass(SIMPLE_VALUE);
+        createEAttribute(simpleValueEClass, SIMPLE_VALUE__VAL);
+
+        objectValueEClass = createEClass(OBJECT_VALUE);
+        createEAttribute(objectValueEClass, OBJECT_VALUE__CLAZZ);
+
+        referencedValueEClass = createEClass(REFERENCED_VALUE);
+        createEAttribute(referencedValueEClass, REFERENCED_VALUE__REF);
+
+        fileValueEClass = createEClass(FILE_VALUE);
+        createEAttribute(fileValueEClass, FILE_VALUE__FILE);
+    }
+
+    /**
+     * <!-- begin-user-doc -->
+     * <!-- end-user-doc -->
+     * @generated
+     */
+    private boolean isInitialized = false;
+
+    /**
+     * Complete the initialization of the package and its meta-model.  This
+     * method is guarded to have no affect on any invocation but its first.
+     * <!-- begin-user-doc -->
+     * <!-- end-user-doc -->
+     * @generated
+     */
+    public void initializePackageContents() {
+        if (isInitialized) return;
+        isInitialized = true;
+
+        // Initialize package
+        setName(eNAME);
+        setNsPrefix(eNS_PREFIX);
+        setNsURI(eNS_URI);
+
+        // Create type parameters
+
+        // Set bounds for type parameters
+
+        // Add supertypes to classes
+        simplePropertyEClass.getESuperTypes().add(this.getProperty());
+        filePropertyEClass.getESuperTypes().add(this.getProperty());
+        complexValueEClass.getESuperTypes().add(this.getAbstractValue());
+        simpleValueEClass.getESuperTypes().add(this.getAbstractValue());
+        objectValueEClass.getESuperTypes().add(this.getComplexValue());
+        referencedValueEClass.getESuperTypes().add(this.getAbstractValue());
+        fileValueEClass.getESuperTypes().add(this.getComplexValue());
+
+        // Initialize classes and features; add operations and parameters
+        initEClass(workflowFileEClass, WorkflowFile.class, "WorkflowFile", !IS_ABSTRACT, !IS_INTERFACE, IS_GENERATED_INSTANCE_CLASS);
+        initEReference(getWorkflowFile_Assignment(), this.getAssignment(), null, "assignment", null, 0, 1, WorkflowFile.class, !IS_TRANSIENT, !IS_VOLATILE, IS_CHANGEABLE, IS_COMPOSITE, !IS_RESOLVE_PROXIES, !IS_UNSETTABLE, IS_UNIQUE, !IS_DERIVED, IS_ORDERED);
+        initEReference(getWorkflowFile_Properties(), this.getProperty(), null, "properties", null, 0, -1, WorkflowFile.class, !IS_TRANSIENT, !IS_VOLATILE, IS_CHANGEABLE, IS_COMPOSITE, !IS_RESOLVE_PROXIES, !IS_UNSETTABLE, IS_UNIQUE, !IS_DERIVED, IS_ORDERED);
+
+        initEClass(propertyEClass, Property.class, "Property", IS_ABSTRACT, !IS_INTERFACE, IS_GENERATED_INSTANCE_CLASS);
+
+        initEClass(simplePropertyEClass, SimpleProperty.class, "SimpleProperty", !IS_ABSTRACT, !IS_INTERFACE, IS_GENERATED_INSTANCE_CLASS);
+        initEAttribute(getSimpleProperty_Name(), ecorePackage.getEString(), "name", null, 0, 1, SimpleProperty.class, !IS_TRANSIENT, !IS_VOLATILE, IS_CHANGEABLE, !IS_UNSETTABLE, !IS_ID, IS_UNIQUE, !IS_DERIVED, IS_ORDERED);
+        initEAttribute(getSimpleProperty_Value(), ecorePackage.getEString(), "value", null, 0, 1, SimpleProperty.class, !IS_TRANSIENT, !IS_VOLATILE, IS_CHANGEABLE, !IS_UNSETTABLE, !IS_ID, IS_UNIQUE, !IS_DERIVED, IS_ORDERED);
+
+        initEClass(filePropertyEClass, FileProperty.class, "FileProperty", !IS_ABSTRACT, !IS_INTERFACE, IS_GENERATED_INSTANCE_CLASS);
+        initEAttribute(getFileProperty_File(), ecorePackage.getEString(), "file", null, 0, 1, FileProperty.class, !IS_TRANSIENT, !IS_VOLATILE, IS_CHANGEABLE, !IS_UNSETTABLE, !IS_ID, IS_UNIQUE, !IS_DERIVED, IS_ORDERED);
+
+        initEClass(assignmentEClass, Assignment.class, "Assignment", !IS_ABSTRACT, !IS_INTERFACE, IS_GENERATED_INSTANCE_CLASS);
+        initEAttribute(getAssignment_ToAssignTo(), ecorePackage.getEString(), "toAssignTo", null, 0, 1, Assignment.class, !IS_TRANSIENT, !IS_VOLATILE, IS_CHANGEABLE, !IS_UNSETTABLE, !IS_ID, IS_UNIQUE, !IS_DERIVED, IS_ORDERED);
+        initEReference(getAssignment_Value(), this.getAbstractValue(), null, "value", null, 0, 1, Assignment.class, !IS_TRANSIENT, !IS_VOLATILE, IS_CHANGEABLE, IS_COMPOSITE, !IS_RESOLVE_PROXIES, !IS_UNSETTABLE, IS_UNIQUE, !IS_DERIVED, IS_ORDERED);
+
+        initEClass(abstractValueEClass, AbstractValue.class, "AbstractValue", IS_ABSTRACT, !IS_INTERFACE, IS_GENERATED_INSTANCE_CLASS);
+
+        initEClass(complexValueEClass, ComplexValue.class, "ComplexValue", IS_ABSTRACT, !IS_INTERFACE, IS_GENERATED_INSTANCE_CLASS);
+        initEAttribute(getComplexValue_Name(), ecorePackage.getEString(), "name", null, 0, 1, ComplexValue.class, !IS_TRANSIENT, !IS_VOLATILE, IS_CHANGEABLE, !IS_UNSETTABLE, !IS_ID, IS_UNIQUE, !IS_DERIVED, IS_ORDERED);
+        initEReference(getComplexValue_Assignment(), this.getAssignment(), null, "assignment", null, 0, -1, ComplexValue.class, !IS_TRANSIENT, !IS_VOLATILE, IS_CHANGEABLE, IS_COMPOSITE, !IS_RESOLVE_PROXIES, !IS_UNSETTABLE, IS_UNIQUE, !IS_DERIVED, IS_ORDERED);
+
+        initEClass(simpleValueEClass, SimpleValue.class, "SimpleValue", !IS_ABSTRACT, !IS_INTERFACE, IS_GENERATED_INSTANCE_CLASS);
+        initEAttribute(getSimpleValue_Val(), ecorePackage.getEString(), "val", null, 0, 1, SimpleValue.class, !IS_TRANSIENT, !IS_VOLATILE, IS_CHANGEABLE, !IS_UNSETTABLE, !IS_ID, IS_UNIQUE, !IS_DERIVED, IS_ORDERED);
+
+        initEClass(objectValueEClass, ObjectValue.class, "ObjectValue", !IS_ABSTRACT, !IS_INTERFACE, IS_GENERATED_INSTANCE_CLASS);
+        initEAttribute(getObjectValue_Clazz(), ecorePackage.getEString(), "clazz", null, 0, 1, ObjectValue.class, !IS_TRANSIENT, !IS_VOLATILE, IS_CHANGEABLE, !IS_UNSETTABLE, !IS_ID, IS_UNIQUE, !IS_DERIVED, IS_ORDERED);
+
+        initEClass(referencedValueEClass, ReferencedValue.class, "ReferencedValue", !IS_ABSTRACT, !IS_INTERFACE, IS_GENERATED_INSTANCE_CLASS);
+        initEAttribute(getReferencedValue_Ref(), ecorePackage.getEString(), "ref", null, 0, 1, ReferencedValue.class, !IS_TRANSIENT, !IS_VOLATILE, IS_CHANGEABLE, !IS_UNSETTABLE, !IS_ID, IS_UNIQUE, !IS_DERIVED, IS_ORDERED);
+
+        initEClass(fileValueEClass, FileValue.class, "FileValue", !IS_ABSTRACT, !IS_INTERFACE, IS_GENERATED_INSTANCE_CLASS);
+        initEAttribute(getFileValue_File(), ecorePackage.getEString(), "file", null, 0, 1, FileValue.class, !IS_TRANSIENT, !IS_VOLATILE, IS_CHANGEABLE, !IS_UNSETTABLE, !IS_ID, IS_UNIQUE, !IS_DERIVED, IS_ORDERED);
+
+        // Create resource
+        createResource(eNS_URI);
+    }
+
+} //WorkflowPackageImpl
