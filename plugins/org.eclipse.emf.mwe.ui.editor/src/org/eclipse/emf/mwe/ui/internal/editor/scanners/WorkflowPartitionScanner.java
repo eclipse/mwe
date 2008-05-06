05d271a5c8c123f06999f09a0a08904c03153507
pschonbac <pschonbac>
2008-05-06 15:28:16 +0000

    Code cleanup

--- plugins/org.eclipse.emf.mwe.ui.editor/src/org/eclipse/emf/mwe/ui/internal/editor/scanners/WorkflowPartitionScanner.java
+++ plugins/org.eclipse.emf.mwe.ui.editor/src/org/eclipse/emf/mwe/ui/internal/editor/scanners/WorkflowPartitionScanner.java
@@ -22,49 +22,48 @@ import org.eclipse.jface.text.rules.Token;
 
 /**
  * @author Patrick Schoenbach
- * @version $Revision: 1.3 $
+ * @version $Revision: 1.4 $
  */
 public class WorkflowPartitionScanner extends RuleBasedPartitionScanner {
-	public static final String XML_START_TAG = "__xml_start_tag";
+    public static final String XML_START_TAG = "__xml_start_tag";
 
-	public static final String XML_END_TAG = "__xml_end_tag";
+    public static final String XML_END_TAG = "__xml_end_tag";
 
-	public static final String XML_PROCESSING_INSTRUCTION =
-			"__xml_processing_instruction";
+    public static final String XML_PROCESSING_INSTRUCTION = "__xml_processing_instruction";
 
-	public static final String XML_DOCTYPE = "__xml_doctype";
+    public static final String XML_DOCTYPE = "__xml_doctype";
 
-	public static final String XML_TEXT = "__xml_text";
+    public static final String XML_TEXT = "__xml_text";
 
-	public static final String XML_COMMENT = "__xml_comment";
+    public static final String XML_COMMENT = "__xml_comment";
 
-	public static final String XML_STRING = "__xml_string";
+    public static final String XML_STRING = "__xml_string";
 
-	public WorkflowPartitionScanner() {
+    public WorkflowPartitionScanner() {
+        final IToken xmlComment = new Token(
+                WorkflowPartitionScanner.XML_COMMENT);
+        final IToken xmlProcessingInstruction = new Token(
+                WorkflowPartitionScanner.XML_PROCESSING_INSTRUCTION);
+        final IToken startTag = new Token(
+                WorkflowPartitionScanner.XML_START_TAG);
+        final IToken endTag = new Token(WorkflowPartitionScanner.XML_END_TAG);
+        final IToken docType = new Token(WorkflowPartitionScanner.XML_DOCTYPE);
+        final IToken text = new Token(WorkflowPartitionScanner.XML_TEXT);
+        final IToken attrString = new Token(
+                WorkflowPartitionScanner.XML_STRING);
 
-		final IToken xmlComment =
-				new Token(WorkflowPartitionScanner.XML_COMMENT);
-		final IToken xmlProcessingInstruction =
-				new Token(WorkflowPartitionScanner.XML_PROCESSING_INSTRUCTION);
-		final IToken startTag =
-				new Token(WorkflowPartitionScanner.XML_START_TAG);
-		final IToken endTag = new Token(WorkflowPartitionScanner.XML_END_TAG);
-		final IToken docType = new Token(WorkflowPartitionScanner.XML_DOCTYPE);
-		final IToken text = new Token(WorkflowPartitionScanner.XML_TEXT);
-        final IToken attrString = new Token(WorkflowPartitionScanner.XML_STRING);
+        final IPredicateRule[] rules = new IPredicateRule[9];
 
-		final IPredicateRule[] rules = new IPredicateRule[9];
-
-		rules[0] = new NonMatchingRule();
-		rules[1] = new MultiLineRule("<!--", "-->", xmlComment);
-		rules[2] = new MultiLineRule("<?", "?>", xmlProcessingInstruction);
-		rules[3] = new MultiLineRule("</", ">", endTag);
-		rules[4] = new StartTagRule(startTag);
-		rules[5] = new MultiLineRule("<!DOCTYPE", ">", docType);
+        rules[0] = new NonMatchingRule();
+        rules[1] = new MultiLineRule("<!--", "-->", xmlComment);
+        rules[2] = new MultiLineRule("<?", "?>", xmlProcessingInstruction);
+        rules[3] = new MultiLineRule("</", ">", endTag);
+        rules[4] = new StartTagRule(startTag);
+        rules[5] = new MultiLineRule("<!DOCTYPE", ">", docType);
         rules[6] = new MultiLineRule("'", "'", attrString);
         rules[7] = new MultiLineRule("\"", "\"", attrString);
-		rules[8] = new TextPredicateRule(text);
+        rules[8] = new TextPredicateRule(text);
 
-		setPredicateRules(rules);
-	}
+        setPredicateRules(rules);
+    }
 }
