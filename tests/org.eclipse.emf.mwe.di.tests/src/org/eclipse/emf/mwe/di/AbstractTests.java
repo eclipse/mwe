e7b5b498d21a9041752a87146bee71f86dcf5378
pschonbac <pschonbac>
2008-06-25 21:39:54 +0000

    Test added

--- tests/org.eclipse.emf.mwe.di.tests/src/org/eclipse/emf/mwe/di/AbstractTests.java
+++ tests/org.eclipse.emf.mwe.di.tests/src/org/eclipse/emf/mwe/di/AbstractTests.java
@@ -4,7 +4,9 @@ import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.mwe.File;
 import org.eclipse.emf.mwe.MwePackage;
 import org.eclipse.emf.mwe.di.execution.Instantiator;
+import org.eclipse.emf.mwe.di.xml.Xml2MweConverter;
 import org.eclipse.xtext.junit.AbstractXtextTests;
+import org.eclipse.xtext.util.StringInputStream;
 
 public abstract class AbstractTests extends AbstractXtextTests {
 
@@ -43,6 +45,16 @@ public abstract class AbstractTests extends AbstractXtextTests {
 	
 		final Instantiator instantiator = new Instantiator();
 		return instantiator.instantiate(file);
+	}
+
+	protected File fileFromXML(final String xmlWorkflow) {
+		if (xmlWorkflow == null)
+			return null;
+	
+		final File file =
+				new Xml2MweConverter().fromXML(new StringInputStream(
+						xmlWorkflow));
+		return file;
 	}
 
 }
--- tests/org.eclipse.emf.mwe.di.tests/src/org/eclipse/emf/mwe/di/xml/Xml2MweConverterTest.java
+++ tests/org.eclipse.emf.mwe.di.tests/src/org/eclipse/emf/mwe/di/xml/Xml2MweConverterTest.java
@@ -2,17 +2,39 @@ package org.eclipse.emf.mwe.di.xml;
 
 import org.eclipse.emf.mwe.File;
 import org.eclipse.emf.mwe.di.AbstractTests;
-import org.eclipse.xtext.util.StringInputStream;
 
 public class Xml2MweConverterTest extends AbstractTests {
-	
+
 	public void testSimple() throws Exception {
-		File file = new Xml2MweConverter().fromXML(new StringInputStream(
-				"<?xml version=\"1.0\"?><workflow id='foo' foo='stuff'><nested value='x'/></workflow>"));
-		
+		final File file =
+				fileFromXML("<?xml version=\"1.0\"?><workflow id='foo' foo='stuff'><nested value='x'/></workflow>");
+		assertNotNull(file);
 		assertWithXtend("2", "value.assignments.size", file);
 		System.out.println(invokeWithXtend("value.assignments.feature", file));
-		assertWithXtend("1", "value.assignments.select(e|e.feature=='nested').size", file);
-		
+		assertWithXtend("1",
+				"value.assignments.select(e|e.feature=='nested').size", file);
+	}
+
+	public void testStandardWorkflow() {
+		final File file =
+				fileFromXML("<workflow>"
+						+ "<property name='modelFile'/>"
+						+ "<property name='targetDir' value='src-gen/'/>"
+						+ "<component file='org/example/dsl/parser/Parser.oaw'>"
+						+ "<modelFile value='${modelFile}'/>"
+						+ "<outputSlot value='theModel'/>"
+						+ "</component>"
+						+ "<component class='oaw.workflow.common.DirectoryCleaner' directories='${targetDir}'/>"
+						+ "<component class='oaw.xpand2.Generator'>"
+						+ "<metaModel id='mm' class='org.eclipse.m2t.type.emf.EmfRegistryMetaModel'/>"
+						+ "<expand value='org::example::dsl::Main::main FOR theModel'/>"
+						+ "<genPath value='${targetDir}'/>" + "</component>"
+						+ "</workflow>");
+		assertNotNull(file);
+		assertWithXtend("3", "value.assignments.size", file);
+		System.out.println(invokeWithXtend("value.assignments.feature", file));
+		assertWithXtend("3",
+				"value.assignments.select(e|e.feature=='component').size",
+				file);
 	}
 }
