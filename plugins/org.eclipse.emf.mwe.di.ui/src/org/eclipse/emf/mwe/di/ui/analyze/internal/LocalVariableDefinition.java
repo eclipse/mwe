fd4759a71fc3a56852696a2e8574a1557755a048
pschonbac <pschonbac>
2008-07-11 14:10:44 +0000

    Reference result container exchanged

--- plugins/org.eclipse.emf.mwe.di.ui/src/org/eclipse/emf/mwe/di/ui/analyze/internal/LocalVariableDefinition.java
+++ plugins/org.eclipse.emf.mwe.di.ui/src/org/eclipse/emf/mwe/di/ui/analyze/internal/LocalVariableDefinition.java
@@ -9,8 +9,8 @@
 
 package org.eclipse.emf.mwe.di.ui.analyze.internal;
 
+import java.util.ArrayList;
 import java.util.Collection;
-import java.util.HashSet;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -21,7 +21,7 @@ import org.eclipse.emf.mwe.Value;
 
 /**
  * @author Patrick Schoenbach - Initial API and implementation
- * @version $Revision: 1.3 $
+ * @version $Revision: 1.4 $
  */
 
 public class LocalVariableDefinition {
@@ -34,12 +34,12 @@ public class LocalVariableDefinition {
 	private final EObject context;
 
 	public LocalVariableDefinition(final LocalVariable variable, final int definitionPosition, final EObject context) {
-		if (variable == null || definitionPosition < 0 || context == null) {
+		if (variable == null || definitionPosition < 0 || context == null)
 			throw new IllegalArgumentException();
-		}
 
-		if (variable.getName() == null || variable.getValue() == null)
+		if (variable.getName() == null || variable.getValue() == null) {
 			throw new IllegalArgumentException("Incomplete variable variable");
+		}
 
 		this.variable = variable;
 		this.definitionPosition = definitionPosition;
@@ -59,7 +59,7 @@ public class LocalVariableDefinition {
 	}
 
 	public Collection<String> getReferences() {
-		final Collection<String> result = new HashSet<String>();
+		final Collection<String> result = new ArrayList<String>();
 		final Matcher m = REFERENCE_PATTERN.matcher(getSimpleValue());
 		while (m.find()) {
 			final String refName = m.group(1);
