3307069f5b8e5b28a0bd3f4e929dc83b57684456
kthoms <kthoms>
2009-01-15 11:44:57 +0000

    Javadoc

--- plugins/org.eclipse.emf.mwe.core/src/org/eclipse/emf/mwe/core/WorkflowInterruptedException.java
+++ plugins/org.eclipse.emf.mwe.core/src/org/eclipse/emf/mwe/core/WorkflowInterruptedException.java
@@ -11,17 +11,24 @@
 
 package org.eclipse.emf.mwe.core;
 
+/**
+ * Exception that signals when a workflow execution should be interrupted due
+ * to an error.
+ */
 public class WorkflowInterruptedException extends RuntimeException {
 
-    /**
-	 * 
-	 */
+    /** The serial version. */
 	private static final long serialVersionUID = 7995016626229631046L;
 
+	/** Creates an instance. */
 	public WorkflowInterruptedException() {
         super();
     }
 
+	/**
+	 * Creates an instance.
+	 * @param message Error message.
+	 */
     public WorkflowInterruptedException(final String message) {
         super(message);
     }
