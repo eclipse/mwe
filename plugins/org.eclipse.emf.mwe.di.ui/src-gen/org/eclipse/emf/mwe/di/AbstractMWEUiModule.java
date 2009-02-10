bb8e16f58c23b748aa25f4af4dd1967048a960e4
pfriese <pfriese>
2009-02-10 22:50:04 +0000

    Avoid double new().

--- plugins/org.eclipse.emf.mwe.di.ui/src-gen/org/eclipse/emf/mwe/di/AbstractMWEUiModule.java
+++ plugins/org.eclipse.emf.mwe.di.ui/src-gen/org/eclipse/emf/mwe/di/AbstractMWEUiModule.java
@@ -6,23 +6,17 @@ package org.eclipse.emf.mwe.di;
 import org.eclipse.xtext.ui.common.service.DefaultUIModule;
 
 /**
- * GENERATED! Manual modification goes to MWEUIModule
+ * Generated from ServiceConfig.xpt!
+ * Manual modifications go to MWEUIModule
  */
 public abstract class AbstractMWEUiModule extends DefaultUIModule {
-	
-	
 	public Class<? extends org.eclipse.xtext.ui.common.editor.contentassist.IProposalProvider> bindIProposalProvider() {
 		return org.eclipse.emf.mwe.di.GenMWEProposalProvider.class;
 	}
-	
 	public Class<? extends org.eclipse.xtext.ui.common.editor.syntaxcoloring.antlr.ITokenColorer> bindITokenColorer() {
 		return org.eclipse.xtext.ui.common.editor.syntaxcoloring.antlr.BuiltinAntlrTokenColorer.class;
 	}
-	
 	public Class<? extends org.eclipse.jface.text.rules.ITokenScanner> bindITokenScanner() {
 		return org.eclipse.xtext.ui.common.editor.syntaxcoloring.antlr.AntlrTokenScanner.class;
 	}
-	
-	
-	
 }
