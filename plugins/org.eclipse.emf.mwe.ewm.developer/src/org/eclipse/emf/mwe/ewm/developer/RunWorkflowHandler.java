428de653b13683acc6e46d775c8a128f11fb6775
bhunt <bhunt>
2009-05-07 03:43:05 +0000

    Launching is now done on a non-ui thread.

--- plugins/org.eclipse.emf.mwe.ewm.developer/src/org/eclipse/emf/mwe/ewm/developer/RunWorkflowHandler.java
+++ plugins/org.eclipse.emf.mwe.ewm.developer/src/org/eclipse/emf/mwe/ewm/developer/RunWorkflowHandler.java
@@ -21,6 +21,10 @@ import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
+import org.eclipse.core.runtime.jobs.Job;
+import org.eclipse.debug.ui.DebugUITools;
 import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.ecore.resource.ResourceSet;
@@ -30,6 +34,7 @@ import org.eclipse.emf.mwe.ewm.workflow.WorkflowComponent;
 import org.eclipse.emf.mwe.ewm.workflow.runtime.WorkflowContext;
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.jface.window.Window;
+import org.eclipse.swt.widgets.Display;
 import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.actions.WorkspaceModifyOperation;
 import org.eclipse.ui.dialogs.ResourceSelectionDialog;
@@ -37,36 +42,37 @@ import org.eclipse.ui.handlers.HandlerUtil;
 
 public class RunWorkflowHandler extends AbstractHandler
 {
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see org.eclipse.core.commands.IHandler#execute(org.eclipse.core.commands.ExecutionEvent)
 	 */
 	public Object execute(ExecutionEvent event) throws ExecutionException
 	{
-		IWorkbenchWindow window = HandlerUtil.getActiveWorkbenchWindow(event);
+		DebugUITools.getSelectedResource();
+		final IWorkbenchWindow window = HandlerUtil.getActiveWorkbenchWindow(event);
 		IStructuredSelection selection = (IStructuredSelection) window.getSelectionService().getSelection();
 		ResourceSelectionDialog dialog = new ResourceSelectionDialog(window.getShell(), ResourcesPlugin.getWorkspace().getRoot(), "Select the workflow context");
-		
-		if(dialog.open() == Window.CANCEL)
+
+		if (dialog.open() == Window.CANCEL)
 			return null;
-		
+
 		ResourceSet resourceSet = new ResourceSetImpl();
 		resourceSet.getLoadOptions().put(XMLResource.OPTION_EXTENDED_META_DATA, Boolean.TRUE);
-		
+
 		IFile workflowFile = (IFile) selection.getFirstElement();
 		URI workflowURI = URI.createPlatformResourceURI(workflowFile.getFullPath().toString(), false);
 		Resource workflowResource = resourceSet.getResource(workflowURI, true);
-		WorkflowComponent workflow = (WorkflowComponent) workflowResource.getContents().get(0);
-		
+		final WorkflowComponent workflow = (WorkflowComponent) workflowResource.getContents().get(0);
+
 		IFile contextFile = (IFile) dialog.getResult()[0];
 		URI contextURI = URI.createPlatformResourceURI(contextFile.getFullPath().toString(), false);
 		final Resource contextResource = resourceSet.getResource(contextURI, true);
-		WorkflowContext context = (WorkflowContext) contextResource.getContents().get(0);
-		
+		final WorkflowContext context = (WorkflowContext) contextResource.getContents().get(0);
+
 		context.setWorkflow(workflow);
-		workflow.start(context);
-		
-		
-		WorkspaceModifyOperation operation = new WorkspaceModifyOperation()
+
+		final WorkspaceModifyOperation operation = new WorkspaceModifyOperation()
 		{
 			@Override
 			protected void execute(IProgressMonitor monitor) throws CoreException, InvocationTargetException, InterruptedException
@@ -83,22 +89,42 @@ public class RunWorkflowHandler extends AbstractHandler
 				}
 			}
 		};
-		
-		try
+
+		Job job = new Job("Workflow: " + workflow.getName())
 		{
-			window.run(true, false, operation);
-		}
-		catch (InvocationTargetException e)
-		{
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		catch (InterruptedException e)
-		{
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
+			@Override
+			protected IStatus run(IProgressMonitor monitor)
+			{
+				workflow.start(context);
+
+				Display.getDefault().asyncExec(new Runnable()
+				{
+					public void run()
+					{
+						try
+						{
+							window.run(true, false, operation);
+						}
+						catch (InvocationTargetException e)
+						{
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						}
+						catch (InterruptedException e)
+						{
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						}
+					}
+				});
+
+				return Status.OK_STATUS;
+			}
+		};
+
+		job.setUser(true);
+		job.schedule();
+
 		return null;
 	}
 }
