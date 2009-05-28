d4540aafd929362788d0b6a897d3fdab0d692c11
pschonbac <pschonbac>
2009-05-28 13:42:49 +0000

    Copyright notice in license headers updated.

--- plugins/org.eclipse.emf.mwe.ui.editor/src/org/eclipse/emf/mwe/ui/internal/editor/format/WorkflowFormattingStrategy.java
+++ plugins/org.eclipse.emf.mwe.ui.editor/src/org/eclipse/emf/mwe/ui/internal/editor/format/WorkflowFormattingStrategy.java
@@ -1,6 +1,6 @@
 /*
  * All rights reserved. This program and the accompanying materials
- * Copyright (c) 2008 committers of openArchitectureWare and others.
+ * Copyright (c) 2009 committers of openArchitectureWare and others.
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
