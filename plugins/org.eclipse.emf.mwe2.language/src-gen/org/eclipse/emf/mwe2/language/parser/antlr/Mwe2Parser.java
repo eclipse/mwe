b77c947ce89d547a5639e39c86a59f7b0a5fc516
szarnekow <szarnekow>
2010-03-07 20:05:48 +0000

    Fix: UnorderedGroups use predicates that allow Antlr to propagate them to calling rules - see https://bugs.eclipse.org/bugs/show_bug.cgi?id=302585

--- plugins/org.eclipse.emf.mwe2.language/src-gen/org/eclipse/emf/mwe2/language/parser/antlr/Mwe2Parser.java
+++ plugins/org.eclipse.emf.mwe2.language/src-gen/org/eclipse/emf/mwe2/language/parser/antlr/Mwe2Parser.java
@@ -26,6 +26,7 @@ public class Mwe2Parser extends org.eclipse.xtext.parser.antlr.AbstractAntlrPars
 		org.eclipse.emf.mwe2.language.parser.antlr.internal.InternalMwe2Parser parser = createParser(tokenStream);
 		parser.setTokenTypeMap(getTokenDefProvider().getTokenDefMap());
 		parser.setSyntaxErrorProvider(getSyntaxErrorProvider());
+		parser.setUnorderedGroupHelper(getUnorderedGroupHelper().get());
 		try {
 			if(ruleName != null)
 				return parser.parse(ruleName);
