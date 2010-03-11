c76cdc7fa1bf00fb1d977d74838cb5d086b3f936
szarnekow <szarnekow>
2010-03-11 21:12:17 +0000

    ContentAssist tweaks for mwe2

--- plugins/org.eclipse.emf.mwe2.language.ui/src/org/eclipse/emf/mwe2/language/ui/contentassist/NamespaceAwareTypesProposalProvider.java
+++ plugins/org.eclipse.emf.mwe2.language.ui/src/org/eclipse/emf/mwe2/language/ui/contentassist/NamespaceAwareTypesProposalProvider.java
@@ -11,14 +11,17 @@ package org.eclipse.emf.mwe2.language.ui.contentassist;
 import java.util.Set;
 
 import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.mwe2.language.mwe2.Module;
 import org.eclipse.emf.mwe2.language.scoping.NamespaceAwareScopeProvider;
 import org.eclipse.jface.text.contentassist.ICompletionProposal;
 import org.eclipse.swt.graphics.Image;
+import org.eclipse.xtext.EcoreUtil2;
 import org.eclipse.xtext.common.types.xtext.ui.JdtTypesProposalProvider;
 import org.eclipse.xtext.scoping.impl.ImportedNamespaceAwareLocalScopeProvider.ImportNormalizer;
 import org.eclipse.xtext.ui.editor.contentassist.ContentAssistContext;
 import org.eclipse.xtext.ui.editor.contentassist.ICompletionProposalAcceptor;
 import org.eclipse.xtext.ui.editor.contentassist.ICompletionProposalFactory;
+import org.eclipse.xtext.ui.editor.contentassist.PrefixMatcher;
 
 import com.google.inject.Inject;
 
@@ -28,7 +31,7 @@ public class NamespaceAwareTypesProposalProvider extends JdtTypesProposalProvide
 	private NamespaceAwareScopeProvider nameSpaceAwareScopeProvider;
 	
 	@Override
-	protected void createTypeProposal(String typeName,int flags, boolean isInnerType, ICompletionProposalFactory proposalFactory, 
+	protected void createTypeProposal(final String typeName,int flags, boolean isInnerType, ICompletionProposalFactory proposalFactory, 
 			ContentAssistContext context, ICompletionProposalAcceptor acceptor) {
 		if (acceptor.canAcceptMoreProposals() && !"java.lang.Object".equals(typeName)) {
 			int lastDot = typeName.lastIndexOf('.');
@@ -36,13 +39,26 @@ public class NamespaceAwareTypesProposalProvider extends JdtTypesProposalProvide
 			if (lastDot != -1)
 				displayString = typeName.substring(lastDot + 1) + " - " + typeName.substring(0, lastDot);
 			Image img = computeImage(typeName,isInnerType, flags);
-			ICompletionProposal proposal = proposalFactory.createCompletionProposal(typeName, displayString, img, context);
-			acceptor.accept(proposal);
+			final PrefixMatcher originalMatcher = context.getMatcher();
+			PrefixMatcher adaptingMatcher = new PrefixMatcher() {
+				@Override
+				public boolean isCandidateMatchingPrefix(String name, String prefix) {
+					return originalMatcher.isCandidateMatchingPrefix(typeName, prefix); 
+				}
+			};
+			try {
+				context.setMatcher(adaptingMatcher);
+				ICompletionProposal proposal = proposalFactory.createCompletionProposal(
+						toShortName(context.getCurrentModel(), typeName), displayString, img, context);
+				acceptor.accept(proposal);
+			} finally {
+				context.setMatcher(originalMatcher);
+			}
 		}
 	}
 	
 	public String toShortName(EObject context, String typeName) {
-		Set<ImportNormalizer> importNormalizer = nameSpaceAwareScopeProvider.getImportNormalizer(context);
+		Set<ImportNormalizer> importNormalizer = nameSpaceAwareScopeProvider.getImportNormalizer(EcoreUtil2.getContainerOfType(context, Module.class));
 		String result = typeName;
 		for(ImportNormalizer normalizer: importNormalizer) {
 			String candidate = normalizer.longToShortName(typeName);
