5704997f6c4e2b7093d57bed4c3e713b4694f255
sefftinge <sefftinge>
2010-05-18 11:54:40 +0000

    added formatting for MWE2

--- plugins/org.eclipse.emf.mwe2.language/src/org/eclipse/emf/mwe2/language/formatting/Mwe2Formatter.java
+++ plugins/org.eclipse.emf.mwe2.language/src/org/eclipse/emf/mwe2/language/formatting/Mwe2Formatter.java
@@ -8,22 +8,56 @@
  *******************************************************************************/
 package org.eclipse.emf.mwe2.language.formatting;
 
+import org.eclipse.emf.mwe2.language.services.Mwe2GrammarAccess;
+import org.eclipse.xtext.Keyword;
+import org.eclipse.xtext.RuleCall;
 import org.eclipse.xtext.formatting.impl.AbstractDeclarativeFormatter;
 import org.eclipse.xtext.formatting.impl.FormattingConfig;
+import org.eclipse.xtext.util.Pair;
 
 /**
  * This class contains custom formatting description.
  * 
- * see : http://www.eclipse.org/Xtext/documentation/latest/xtext.html#formatting
- * on how and when to use it 
+ * see : http://www.eclipse.org/Xtext/documentation/latest/xtext.html#formatting on how and when to use it
  * 
  * Also see {@link org.eclipse.xtext.xtext.XtextFormattingTokenSerializer} as an example
  */
 public class Mwe2Formatter extends AbstractDeclarativeFormatter {
-	
+
 	@Override
 	protected void configureFormatting(FormattingConfig c) {
+		Mwe2GrammarAccess f = (Mwe2GrammarAccess) getGrammarAccess();
 
-		//...
+		c.setAutoLinewrap(120);
+
+		RuleCall moduleName = f.getModuleAccess().getCanonicalNameFQNParserRuleCall_2_0();
+		c.setLinewrap(2).between(moduleName, f.getImportRule());
+		c.setLinewrap(2).between(moduleName, f.getDeclaredPropertyRule());
+		c.setLinewrap(2).between(moduleName, f.getRootComponentRule());
+
+		c.setLinewrap(2).between(f.getImportRule(), f.getDeclaredPropertyRule());
+		c.setLinewrap(2).between(f.getImportRule(), f.getRootComponentRule());
+		c.setLinewrap(1,1,2).after(f.getImportRule());
+
+		c.setLinewrap(2).between(f.getDeclaredPropertyRule(), f.getRootComponentRule());
+		c.setLinewrap().after(f.getDeclaredPropertyRule());
+
+		c.setLinewrap().after(f.getAssignmentRule());
+
+		// Parenthesis
+		for (Pair<Keyword, Keyword> pair : f.findKeywordPairs("{", "}")) {
+			c.setLinewrap(1,1,2).after(pair.getFirst());
+			c.setLinewrap(1,1,2).after(pair.getSecond());
+			c.setIndentation(pair.getFirst(), pair.getSecond());
+			c.setNoSpace().between(pair.getFirst(), pair.getSecond());
+		}
+
+		// No space in property refs.
+		c.setNoSpace().around(f.getPropertyReferenceRule());
+
+		// Keep some linewraps before/after comments.
+		c.setLinewrap(0, 1, 2).before(f.getSL_COMMENTRule());
+		c.setLinewrap(0, 1, 2).before(f.getML_COMMENTRule());
+		c.setLinewrap(0, 1, 1).after(f.getML_COMMENTRule());
 	}
 }
