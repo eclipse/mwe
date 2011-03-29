da2546c60e1fb86ef7f6835660c799cd8b8238ce
Sven Efftinge <sven.efftinge@itemis.de>
2011-03-29 13:56:49 +0200

    Added protected getter for org.eclipse.emf.mwe2.runtime.workflow.AbstractCompositeWorkflowComponent.children (see https://bugs.eclipse.org/bugs/show_bug.cgi?id=341207)

--- plugins/org.eclipse.emf.mwe2.runtime/src/org/eclipse/emf/mwe2/runtime/workflow/AbstractCompositeWorkflowComponent.java
+++ plugins/org.eclipse.emf.mwe2.runtime/src/org/eclipse/emf/mwe2/runtime/workflow/AbstractCompositeWorkflowComponent.java
@@ -16,24 +16,28 @@ public abstract class AbstractCompositeWorkflowComponent implements
 		IWorkflowComponent {
 	private List<IWorkflowComponent> children = new ArrayList<IWorkflowComponent>();
 	
+	protected List<IWorkflowComponent> getChildren() {
+		return children;
+	}
+	
 	public void addComponent(IWorkflowComponent component) {
 		this.children.add(component);
 	}
 
 	public void preInvoke() {
-		for (IWorkflowComponent component : children) {
+		for (IWorkflowComponent component : getChildren()) {
 			component.preInvoke();
 		}
 	}
 
 	public void invoke(IWorkflowContext ctx) {
-		for (IWorkflowComponent component : children) {
+		for (IWorkflowComponent component : getChildren()) {
 			component.invoke(ctx);
 		}
 	}
 
 	public void postInvoke() {
-		for (IWorkflowComponent component : children) {
+		for (IWorkflowComponent component : getChildren()) {
 			component.postInvoke();
 		}
 	}
